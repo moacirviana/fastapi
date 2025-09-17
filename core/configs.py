468701964d56ffad8afcc63c38dd69d3fc08a589
015697172275 <moacir.viana@tre-am.jus.br>
2025-09-17 08:08:13 -0400

    mudanca do db para raiz do projeto

--- core/configs.py
+++ core/configs.py
@@ -6,7 +6,7 @@ from typing import ClassVar
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     #DB_URL: str = "sqlite+aiosqlite:///./Users/moacirmoacir/dev/db/xibefood.db3"
-    DB_URL: str = "sqlite+aiosqlite:////Users/moacirmoacir/dev/db/xibefood.db3"
+    DB_URL: str = "sqlite+aiosqlite:///./xibefood.db3"
     DBBaseModel : ClassVar[DeclarativeMeta] =  declarative_base()
 
     JWT_SECRET : str = 'seLtKtzdkPp1FhUvQaXBjxgqbhp3iKIK'
